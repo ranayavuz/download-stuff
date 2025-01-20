5d5ad3770a1351b760fb7dff39174cb07561ca50
Rana <rana.yavuz@thekaustschool.org>
2025-01-20 09:01:50 +0300

    Update fourcorners.cpp

--- fourcorners.cpp
+++ fourcorners.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 int main() {
+  srand(time(0));
+  int choice = rand() % 5;
 cout << "four corners\n";
+  cout << "choose a corner: ";
   int ans;
   cin >> ans;
+  corners[4] = [1, 2, 3, 4];
+  for(string i : corners) {
+    if(!ans == i) {
+      cout << "your cheating arent you?";
+      cout << 1/0;
+    }
+  }
+  
 }
